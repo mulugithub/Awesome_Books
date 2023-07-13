8b5726e336d210170cfb0c5d0afc5a1a7b782b38
Muluken Zemed <zmmulu@gmail.com>
2023-07-13 10:56:45 +0300

    Add navbar section and footer section

--- scripts.js
+++ scripts.js
@@ -137,3 +137,28 @@ addButton.addEventListener('click', (event) => {
 Books.loadBooks().forEach((book) => {
   Books.renderBooks(book.id, book.title, book.author);
 });
+
+// Get the navigation links and content sections
+const navLinks = document.querySelectorAll('header nav a');
+const contentSections = document.querySelectorAll('.content-section');
+
+// Add click event listeners to the navigation links
+navLinks.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    // Prevent the default link behavior
+    event.preventDefault();
+
+    // Get the ID of the clicked link
+    const sectionId = event.target.getAttribute('href');
+
+    // Remove the "active" class from all links and content sections
+    navLinks.forEach((link) => link.classList.remove('active'));
+    contentSections.forEach((section) => section.classList.add('hidden'));
+
+    // Add the "active" class to the clicked link
+    event.target.classList.add('active');
+
+    // Show the corresponding content section
+    document.querySelector(sectionId).classList.remove('hidden');
+  });
+});
