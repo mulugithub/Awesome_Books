b138abb0f9b062d3132651606fd54620c2970b25
anyars-encarta <anyarsencarta@gmail.com>
2023-07-13 07:59:35 +0000

    Create date function

--- scripts.js
+++ scripts.js
@@ -162,3 +162,18 @@ navLinks.forEach((link) => {
     document.querySelector(sectionId).classList.remove('hidden');
   });
 });
+
+const dateTime = () => {
+  const date = new Date();
+  const options = {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+    weekday: 'short',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour24: true,
+  };
+  return date.toLocaleString('en-US', options);
+};
